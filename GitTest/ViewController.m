64e27870d3f77d9651457f5b64a0b8d5a833ca68
cxlww <952688183@qq.com>
2016-05-26 10:08:10 +0800

    第一次提交。增加的第一行

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"这是第一行代码");
 }
 
 - (void)didReceiveMemoryWarning {
